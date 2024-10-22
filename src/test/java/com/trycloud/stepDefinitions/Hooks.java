a49d13813a0bcfcd0decdb47eeaa084511bc5c6a
green <greenfreez@gmail.com>
2024-10-22 11:23:19 -0700

    Driver class Import problems

---
+++ src/test/java/com/trycloud/stepDefinitions/Hooks.java
@@ -0,0 +1,37 @@
+package com.trycloud.stepDefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.time.Duration;
+
+import static com.TryCloud.utility.Driver.getDriver;
+
+public class Hooks {
+
+    //import the @Before coming from io.cucumber.java
+    @Before(order = 1)
+    public void setupMethod() {
+
+     getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        getDriver().get(com.TryCloud.utility.ConfigurationReader.getProperty("url"));
+    }
+
+    /*
+    @After will be executed automatically after EVERY scenario in the project.
+     */
+    @After
+    public void teardownMethod(Scenario scenario) {
+
+        if (scenario.isFailed()) {
+
+            byte[] screenshot = ((TakesScreenshot) com.TryCloud.utility.Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+
+        }
+    }
+}
