29e60d17d0bdb2f2019ee5bda12e8a6b25b4d758
ersuamine <amine.ersu@gmail.com>
2024-11-05 23:59:59 -0500

    US06 Feature file ready, step definitions set for implementation

---
+++ src/test/java/com/trycloud/stepDefinitions/TalkModule_stepDef_AE.java
@@ -0,0 +1,63 @@
+package com.trycloud.stepDefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TalkModule_stepDef_AE {
+
+    @When("user clicks Create new conversation button")
+    public void user_clicks_create_new_conversation_button() {
+
+    }
+
+    @When("user enters conversation name {string}")
+    public void user_enters_conversation_name(String conversationName) {
+
+    }
+
+    @When("user clicks join via link checkbox")
+    public void user_clicks_join_via_link_checkbox() {
+
+    }
+
+    @When("user clicks add participants button")
+    public void user_clicks_add_participants_button() {
+
+    }
+
+    @When("user selects participants")
+    public void user_selects_participants() {
+
+    }
+
+    @When("user clicks Create conversation button")
+    public void user_clicks_create_conversation_button() {
+
+    }
+
+    @Then("user should see {string} conversation under the list")
+    public void user_should_see_conversation_under_the_list(String conversationName) {
+
+    }
+
+    @When("user opens the elipses menu")
+    public void user_opens_the_elipses_menu() {
+
+    }
+
+    @When("user clicks {string} from the menu")
+    public void user_clicks_from_the_menu(String menuItem) {
+
+    }
+
+    @When("user chooses {string} from the popup alert")
+    public void user_chooses_from_the_popup_alert(String choice) {
+
+    }
+
+    @Then("user should not see the conversation under the list")
+    public void user_should_not_see_the_conversation_under_the_list() {
+
+    }
+
+}
