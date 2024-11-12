1b042a2a52f1c2d02e38a7c41ce0c1a10dfe39c6
teribogard <amine.ersu@gmail.com>
2024-11-12 00:52:48 -0500

    Revert "Test"
    This reverts commit 3c5abf677e63ba021b4f3a51dd455b941d6a8718.

--- src/test/java/com/trycloud/runners/CukesRunner.java
+++ src/test/java/com/trycloud/runners/CukesRunner.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/trycloud/stepDefinitions",
-        dryRun = true,
-        tags = "@US06",
+        dryRun = false,
+        tags = "@us01",
         publish = true // generating a report with public link
         // monochrome = false
 
