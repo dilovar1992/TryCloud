3570e1f15de8a7d5836677020f292759f0a53c9a
ersu-amine <ersu-amine@gmail.com>
2024-11-28 02:55:57 -0500

    second ac

--- src/test/java/com/trycloud/pages/TalkPage.java
+++ src/test/java/com/trycloud/pages/TalkPage.java
@@ -1,11 +1,13 @@
 package com.trycloud.pages;
 
+import com.trycloud.utility.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
 
-public class TalkPage extends BasePage{
+public class TalkPage extends BasePage {
     @FindBy(xpath = "//button[@slot='trigger']")
     public WebElement newConversationButton;
 
@@ -28,16 +30,55 @@ public class TalkPage extends BasePage{
     public WebElement closeButton;
 
     @FindBy(xpath = "(//ul[@class='conversations']//a)[1]") //new conversation is always the 1st item
-    public WebElement conversation;
+    public WebElement latestConversation;
+
+    @FindBy(xpath = "//ul[@class='conversations']//a") //new conversation is always the 1st item
+    public List<WebElement> allConversation;
+
+    @FindBy(xpath = "//div[@class='oc-dialog']//button[2]")
+    public WebElement alertYesButton;
 
     /**
-     *
      * @return aria-label attribute value, which contains the conversation name in format "Conversation, conversationName"
      */
-    public String getConversationName(){
-        return conversation.getAttribute("aria-label");
+    public String getConversationName(WebElement element) {
+        return element.getAttribute("aria-label");
     }
 
+    public WebElement getNewConversationEllipses(String conversationName) {
 
+        return Driver.getDriver().findElement(By.xpath("//a[@aria-label='Conversation, " + conversationName + "']/..//button"));
+    }
 
+    public WebElement getMenuItem(String itemText) {
+
+        int i = 1;
+        switch (itemText) {
+            case "Add to favorites":
+                i = 1;
+                break;
+            case "Copy link":
+                i = 2;
+                break;
+            case "All messages":
+                i = 3;
+                break;
+            case "@-mentions only":
+                i = 4;
+                break;
+            case "Off":
+                i = 5;
+                break;
+            case "Leave conversation":
+                i = 6;
+                break;
+            case "Delete conversation":
+                i = 7;
+                break;
+        }
+
+        //TODO fix element, giving no such element exception
+        WebElement item = Driver.getDriver().findElement(By.xpath("(//ul[@id='menu-cckbf']//span[2])[i]"));
+        return item;
+    }
 }
