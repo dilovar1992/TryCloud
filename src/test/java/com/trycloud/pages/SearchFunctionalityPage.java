c2c02aa5b4e8d82b988725703c2e2e6b2106ef02
dilovar1992 <dellovar56@gmail.com>
2025-02-08 13:08:12 -0500

    US08 Test cases 1 is complete

---
+++ src/test/java/com/trycloud/pages/SearchFunctionalityPage.java
@@ -0,0 +1,22 @@
+package com.trycloud.pages;
+
+import com.trycloud.utility.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SearchFunctionalityPage extends BasePage{
+    @FindBy(xpath = "//a[@aria-controls='header-menu-unified-search']/span")
+    public WebElement searchButton;
+
+    @FindBy(xpath = "//input[@type='search']")
+    public WebElement searchInput;
+
+    public WebElement searchResults(String filename){
+        return Driver.getDriver().findElement(By.xpath("//a//span[@class='unified-search__result-content']//strong[.='"+filename+"']"));
+    }
+    public WebElement file(String file){
+        return Driver.getDriver().findElement(By.xpath("//a[@class='name']//span[.='"+file+"']"));
+    }
+
+}
